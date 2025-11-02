771f9ce4d56d99e0cfab68543367946808e0238b
hathor1411 <47355454+hathor1411@users.noreply.github.com>
2025-11-02 09:18:11 +0100

    Tag 2: React-Frontend initialisiert

---
+++ frontend/src/App.jsx
@@ -0,0 +1,8 @@
+export default function App() {
+  return (
+    <div style={{ textAlign: "center", marginTop: "2rem" }}>
+      <h1>MotoTourer 🏍️</h1>
+      <p>Willkommen zu deiner Routenplaner-App!</p>
+    </div>
+  );
+}
