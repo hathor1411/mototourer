17c6a25897b5bb163b056b2a0da72986ffc7e395
hathor1411 <47355454+hathor1411@users.noreply.github.com>
2025-11-06 22:54:20 +0100

    Github workflow Prüfung ( Tag 14.1)

--- backend/tests/test_routes.py
+++ backend/tests/test_routes.py
@@ -1,5 +1,9 @@
-from fastapi.testclient import TestClient
+import sys, os
+sys.path.append(os.path.abspath(os.path.join(os.path.dirname(__file__), "..")))
+
 from main import app
+from fastapi.testclient import TestClient
+
 
 client = TestClient(app)
 
